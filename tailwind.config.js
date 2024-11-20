f07c962feb0b8f7d3e32f45776be0650d796e88c
ram-kumar77 <ramkumarsundar07@gmail.com>
2024-11-20 02:34:15 -0800

    Add files via upload
    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}", './src/components/**/*.jsx'],
+  theme: {
+    extend: {
+      fontFamily: {
+        customFont1: ['"Righteous"', "sans-serif"],
+        customFont2: ['"saira"', "sans-serif"],
+        // Add more custom font families as needed
+      },
+    },
+  },
+  plugins: [],
+}
+
